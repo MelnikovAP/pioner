87574d36771a195e479c7831095a7e07a609a02a
Evgeniy Komov <evgenkomov@gmail.com>
2022-07-04 17:00:31 +0300

    minor fixes

--- ai_device.py
+++ ai_device.py
@@ -1,4 +1,9 @@
+from typing import Tuple
+from ctypes import Array
+
 import uldaq as ul
+import logging
+
 
 class AiParams:
     def __init__(self):
@@ -6,12 +11,13 @@ class AiParams:
         self.range_id = -1
         self.low_channel = -1
         self.high_channel = -1
-        self.input_mode = 2  # ul.AiInputMode.SINGLE_ENDED
-        self.scan_flags = 0  # ul.AInScanFlag.DEFAULT
-        self.options = 8  # ul.ScanOption.CONTINUOUS by default
+        self.input_mode = ul.AiInputMode.SINGLE_ENDED  # 2
+        self.scan_flags = ul.AInScanFlag.DEFAULT  # 0
+        self.options = ul.ScanOption.CONTINUOUS  # 8
 
     def __str__(self):
         return str(vars(self))
+
 
 class AiDeviceHandler:
     """Wraps the analog-input uldaq.AiDevice."""
@@ -28,33 +34,41 @@ class AiDeviceHandler:
         """
         self._ai_device = ai_device_from_daq
         self._params = params
+        self._check_device()
+        self._init_buffer()
 
+    # TODO: maybe check it right after connecting to the DaqDevice ?
+    def _check_device(self):
         if self._ai_device is None:
-            logging.error("Error. DAQ device doesn't support analog input.")
-            raise RuntimeError("Error. DAQ device doesn't support analog input.")
+            error_str = "Error. DAQ device doesn't support analog input."
+            logging.error(error_str)
+            raise RuntimeError(error_str)
 
         info = self._ai_device.get_info()
         if not info.has_pacer():
-            logging.error("Error. DAQ device doesn't support hardware paced analog input.")
-            raise RuntimeError("Error. DAQ device doesn't support hardware paced analog input.")
+            error_str = "Error. DAQ device doesn't support hardware paced analog input."
+            logging.error(error_str)
+            raise RuntimeError(error_str)
 
         if info.get_num_chans_by_mode(ul.AiInputMode.SINGLE_ENDED) <= 0:
             self._params.input_mode = ul.AiInputMode.DIFFERENTIAL
 
-        self.channel_count = self._params.high_channel - self._params.low_channel + 1
-        self._buffer = ul.create_float_buffer(self.channel_count, self._params.sample_rate)
+    def _init_buffer(self):
+        channel_count = self._params.high_channel - self._params.low_channel + 1
+        self._buffer = ul.create_float_buffer(channel_count, self._params.sample_rate)
 
     def get(self) -> ul.AiDevice:
         """Provides explicit access to the uldaq.AiDevice."""
         return self._ai_device                                   
 
-    def data(self):
+    def get_buffer(self) -> Array[float]:
+        """Returns an array of double precision floating point sample values."""
         return self._buffer
 
     def stop(self):
         self._ai_device.scan_stop()
   
-    def status(self):
+    def status(self) -> Tuple[ul.ScanStatus, ul.TransferStatus]:
         return self._ai_device.get_scan_status()
 
     # returns actual input scan rate
