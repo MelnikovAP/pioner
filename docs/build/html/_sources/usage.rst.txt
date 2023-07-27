22abdca1b1360c4c16fed7fccaba8601ed5b68bc
MelnikovAP <melnikov.al.pe@gmail.com>
2023-07-27 12:57:42 +0200

    basic sphinx docs in=tegration done

---
+++ docs/build/html/_sources/usage.rst.txt
@@ -0,0 +1,19 @@
+Usage
+=====
+
+Installation
+------------
+
+To use Lumache, first install it using pip:
+
+.. code-block:: console
+
+   (.venv) $ pip install lumache
+
+
+Run GUI
+------------
+
+you can use the ``lumache.get_random_ingredients()`` function:
+
+.. autofunction:: runUI.pioner_run_ui
--- src/pioner/runUI.py
+++ src/pioner/runUI.py
@@ -4,6 +4,16 @@ import sys
 from pioner.front.mainWindow import mainWindow
 
 def pioner_run_ui():
+    """
+    Test documentation.
+
+    :param kind: Optional "kind" of ingredients.
+    :type kind: list[str] or None
+    :raise lumache.InvalidKindError: If the kind is invalid.
+    :return: The ingredients list.
+    :rtype: list[str]
+
+    """
     app = qt.QApplication([])
     sys.excepthook = qt.exceptionHandler
     app.setStyle('Fusion')
